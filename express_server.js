a5df2a431ffac9bdafc897939f1f795b2e23b71b
Garo-A <garo.assilian@gmail.com>
2017-07-07 21:24:24 +0000

    Edited comments

--- express_server.js
+++ express_server.js
@@ -2,22 +2,23 @@ const express = require ('express');
 const bodyParser = require("body-parser");
 const cookieSession = require ('cookie-session');
 const bcrypt = require ('bcrypt');
-const methodOverride = require ('method-override')
+const methodOverride = require ('method-override');
 const app = express();
 
 
 
-app.use(bodyParser.urlencoded({extended: true})); //Using body parser
-app.set('view engine', 'ejs'); //Sets the view engine to render EJS files
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 app.use(cookieSession({
   name: 'session',
   keys: ["key1"]
-}))
+}));
 app.use(methodOverride('_method'));
 
-//Setting default port.
+
 const PORT = 8080;
 
+//Contains information about URLs.
 let urlDatabase = {
   "b2xVn2": {
     short: 'b2xVn2',
@@ -33,7 +34,7 @@ let urlDatabase = {
   }
 };
 
-
+//Contains infromation about users.
 let users = {
   "1": {
     id: 1,
@@ -47,43 +48,41 @@ let users = {
   }
 }
 
-// Will basically generate a random number, round down, multiply by charset length, get approprate charset letter and return a string of 5 chars long.
+// Generates random number.
 function generateRandomString() {
   let text = "";
   const charset = "abcdefghijklmnopqrstuvwxyz0123456789";
     for( var i = 0; i < 5; i++ ) {
       text += charset.charAt(Math.floor(Math.random() * charset.length));
-    }
+    };
   return text;
-}
+};
 
-// This will go throguh urlDatabase and look for the urls containing a specific user ID. It will then return a new object with those URLs.
+// Returns an array containing a given user's generated URLs.
 function URLUser(id) {
   let all = {};
   for (let url in urlDatabase){
     if (id == urlDatabase[url].userID) {
       all[url] = urlDatabase[url];
-    }
-  }
+    };
+  };
   return all;
-}
+};
 
 
 let errorMessage = "no error";
 let registrationError = "no error";
 
-
-// Express starts hosting the routrs HERE.
+// Home - Redirects to URLs Index if there is a user logged in, if not goes to Login page.
 app.get("/", function(req, res) {
   if (req.session.user_id === undefined) {
-    res.redirect('/login')
+    res.redirect('/login');
   } else {
     res.redirect('/urls');
-  }
-
+  };
 })
 
-// Shows what's currently in the urlDatabase
+// Shows URLs for a user that is logged in.
 app.get('/urls', function(req,res){
 
     let user = req.session.user_id;
@@ -91,33 +90,33 @@ app.get('/urls', function(req,res){
       urls: URLUser(user),
       username: users[req.session.user_id]
     });
-})
+});
 
-//Takes whatver user put into urls/new page and loads into urlDatabase. Will then redirect to that short url's page
+//Adds a new, short URL to database with ID of user that created it.
 app.post('/urls', function(req,res){
   let newURL = (req.body.longURL);
-  let short = generateRandomString()
+  let short = generateRandomString();
   urlDatabase[short] = {};
   urlDatabase[short].short = short;
   urlDatabase[short].long = newURL;
   urlDatabase[short].userID = req.session.user_id;
   urlDatabase[short].count = 0;
 
-  let path = `urls/${short}`
+  let path = `urls/${short}`;
   res.redirect(path);
 })
 
 
-// Will ask input for long URL
+//Shows input field to shorten new URL.
 app.get('/urls/new', (req, res) => {
   if (req.session.user_id !== undefined){
     res.render('urls_new', {
       username: users[req.session.user_id]
-    })
+    });
   } else {
     res.redirect('/login');
-  }
-})
+  };
+});
 
 //Responsible for taking short URL and redirecting to actual webpage
 app.get('/u/:shortU', function (req,res){
@@ -138,19 +137,19 @@ app.get('/register', function(req,res){
   if (req.session.user_id === undefined){
     res.render('register_page', {
       registrationError: registrationError
-    })
+    });
   } else {
     res.redirect('/urls');
-  }
-})
+  };
+});
 
 app.get('/login', function(req,res){
   res.render('login_page', {
     errorMessage: errorMessage
-  })
-})
+  });
+});
 
-//Shows long URL of given short URL
+//Shows a view page for a given short URL and all data associated to it. Also allows user who creted URL to edit it.
 app.get('/urls/:id', function(req,res){
   if (req.session.user_id === undefined){
     res.redirect('/urls');
@@ -159,24 +158,24 @@ app.get('/urls/:id', function(req,res){
       shortURL: req.params.id,
       urls: urlDatabase,
       username: users[req.session.user_id]
-    })
-  }
-})
+    });
+  };
+});
 
-//Shows individual URL page
+//Allows a given user to modify a URL that belongs to them.
 app.put('/urls/:id', function(req,res){
   let updatedURL = (req.body.updatedURL);
   urlDatabase[req.params.id].long = updatedURL;
   res.redirect('/urls');
-})
+});
 
-//This ONLY executes when there's a POST request sent to this SPECIFIC path.
+//Allows user to delete a created URL.
 app.delete('/urls/:id/delete', function(req, res){
   delete urlDatabase[req.params.id];
-  res.redirect('/urls')
-})
+  res.redirect('/urls');
+});
 
-//Gets Username from input form and stores in request cookies header
+//Checks login information with user database, and if everything matches, sets cookies.
 app.post('/login', function(req,res){
   let userEmail = req.body.email;
   let userPassword = req.body.password;
@@ -192,23 +191,23 @@ app.post('/login', function(req,res){
 
         else {
           res.statusCode = 403;
-        }
+        };
       }
     else {
       res.statusCode = 403;
-    }
-  }
+    };
+  };
 
   if (res.statusCode === 403) {
     errorMessage = "Error";
     res.render('login_page', {
       errorMessage: errorMessage
     });
-  }
+  };
 
-})
+});
 
-//Will logout user and reset uername cookie to underfined
+//Allows user to logout, resets cookies.
 app.post('/logout', function(req,res){
   errorMessage = "no error";
   req.session = null;
@@ -232,10 +231,10 @@ app.post('/register', function(req,res){
   }
 
   if (res.statusCode === 400) {
-    registrationError = "error"
+    registrationError = "error";
     res.render('register_page', {
       registrationError: registrationError
-    })
+    });
 
   } else {
       users[newID] = {};
@@ -244,12 +243,12 @@ app.post('/register', function(req,res){
       users[newID].password = bcrypt.hashSync(req.body.password, 10);
 
       registrationError = "no error";
-      res.redirect('/urls')
+      res.redirect('/urls');
   }
-})
+});
 
 
-// This is used to actually start the server.
+
 app.listen(PORT, function (){
   console.log(`Listening on port ${PORT}`);
 })
